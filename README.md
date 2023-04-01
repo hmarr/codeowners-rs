2f3c440a4b8c7fd8f94f6347c7cbe0b840747741
Harry Marr <harry.marr@gmail.com>
2023-04-01 13:15:16 -0400

    Update docs, remove caveat about parser

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
 # codeowners-rs
 
-A Rust library and CLI for GitHub's [CODEOWNERS file](https://docs.github.com/en/github/creating-cloning-and-archiving-repositories/about-code-owners#codeowners-syntax).
+A fast Rust library and CLI for GitHub's [CODEOWNERS file](https://docs.github.com/en/github/creating-cloning-and-archiving-repositories/about-code-owners#codeowners-syntax).
 
-Still a work in progress. The parser is particularly inadequate right now.
+## CLI usage
 
-The set of patterns is turned into an NFA, which makes matching fast even when there are a large number of patterns.
+```
+$ codeowners --help
+Usage: codeowners [OPTIONS] [PATHS]...
+
+Arguments:
+  [PATHS]...
+
+Options:
+  -f, --file <CODEOWNERS_FILE>
+          Path to a CODEOWNERS file. If omitted, the following locations will be tried: ./CODEOWNERS, ./.github/CODEOWNERS
+  -p, --paths-from <PATHS_FROM_FILE>
+          Match paths from this file rather than walking the directory tree
+  -o, --owners <OWNERS>
+          Filter results to files owned by this owner. May be used multiple times to match multiple owners
+  -u, --unowned
+          Filter results to show unowned files. May be used with -o
+  -t, --threads <THREADS>
+          Concurrency. If set to 0, a sensible value based on CPU count will be used [default: 0]
+  -h, --help
+          Print help information
+  -V, --version
+          Print version information
+```
--- codeowners-rs/src/lib.rs
+++ codeowners-rs/src/lib.rs
@@ -1,18 +1,30 @@
-//! This is a library for working with GitHub [CODEOWNERS files](https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/about-code-owners).
+//! This is a library for working with GitHub [CODEOWNERS
+//! files](https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/about-code-owners).
 //! CODEOWNERS files declare which GitHub users or teams are responsible for
 //! files in a repository. The pattern syntax is loosely based on the glob-style
 //! patterns used in .gitignore files.
 //!
-//! ## Warning: this is a work in progress
-//! This project is in an early state, and most of the work has gone into making
-//! the matching performant so that large numbers of files and rules can be
-//! processed quickly. A CODEOWNERS file parser is included, but it's terrible
-//! and needs a rewrite—it accepts invalid files without error, and fails to
-//! parse some valid files.
+//! ## Parsing CODEOWNERS files
+//! The [`parse`] and [`parse_file`] functions can be used to parse a CODEOWNERS
+//! file into a [`parser::ParseResult`], which wraps a vec of [`parser::Rule`]s
+//! and a vec of any [`parser::ParseError`]s that were encountered. The
+//! [`parser::Rule`] struct contains full syntactic information about the rule,
+//! including the leading and trailing comments, and the byte offsets for each
+//! component of the rule. This is useful when you care about the CODEOWNERS
+//! syntax, for instance when writing a syntax highlighter, but it's not very
+//! ergonomic for most use cases.
 //!
-//! ## Command line interface
-//! There is a companion binary crate that provides a simple CLI for matching
-//! paths against a CODEOWNERS file.
+//! The [`RuleSet`] struct provides a more ergonomic interface for working with
+//! CODEOWNERS files. It can be constructed by calling
+//! [`into_ruleset`](fn@parser::ParseResult::into_ruleset) on a
+//! [`parser::ParseResult`].
+//!
+//! ## Matching paths against CODEOWNERS files
+//! The [`RuleSet`] struct provides a [`owners`](fn@RuleSet::owners) method that
+//! can be used to match a path against a CODEOWNERS file. To get the matching
+//! rule rather than just the owners, use the
+//! [`matching_rule`](fn@RuleSet::matching_rule) method.
+//!
 //!
 //! ## Example
 //! ```
@@ -28,6 +40,10 @@
 //!    println!("{}: {:?}", path, owners);
 //! }
 //! ```
+//!
+//! ## Command line interface
+//! There is a companion binary crate that provides a simple CLI for matching
+//! paths against a CODEOWNERS file.
 
 pub mod parser;
 pub mod patternset;
